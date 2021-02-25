3d3a30ada7a80878b6c6a80c8c337551872e9d5d
abc <liukaijv@foxmail.com>
2021-02-25 10:57:55 +0800

    bio udp

--- bio-udp/src/main/java/com/exampe/udp/Client.java
+++ bio-udp/src/main/java/com/exampe/udp/Client.java
@@ -1,8 +1,47 @@
 package com.exampe.udp;
 
+import java.io.IOException;
+import java.net.*;
+
 public class Client {
 
+    private static final int PORT = 4444;
+
     public static void main(String[] args) {
+
+        DatagramSocket socket = null;
+        byte[] buf = new byte[1024];
+
+        try {
+
+            socket = new DatagramSocket();
+
+            for (int i = 0; i < 1; i++) {
+
+                byte[] sendData = ("hello_" + i).getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("localhost"), PORT);
+                socket.send(sendPacket);
+                System.out.println("send data: " + new String(sendData));
+
+                DatagramPacket receivePacket = new DatagramPacket(buf, buf.length);
+                socket.receive(receivePacket);
+                byte[] data = receivePacket.getData();
+                String message = new String(data, 0, receivePacket.getLength());
+                System.out.println("receive data: " + message);
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
 
     }
 }
--- bio-udp/src/main/java/com/exampe/udp/Server.java
+++ bio-udp/src/main/java/com/exampe/udp/Server.java
@@ -1,7 +1,54 @@
 package com.exampe.udp;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class Server {
+
+    private static final int PORT = 4444;
+
     public static void main(String[] args) {
 
+        DatagramSocket socket = null;
+
+        try {
+            socket = new DatagramSocket(PORT);
+            System.out.println("server start at: " + socket.getLocalAddress() + ":" + socket.getLocalPort());
+            byte[] buf = new byte[1024];
+
+            while (true) {
+
+                DatagramPacket receivePacket = new DatagramPacket(buf, buf.length);
+
+                // 阻塞等待接收客户端数据
+                socket.receive(receivePacket);
+
+                byte[] data = receivePacket.getData();
+                String message = new String(data, 0, receivePacket.getLength());
+
+                InetAddress address = receivePacket.getAddress();
+                int port = receivePacket.getPort();
+                System.out.println("receive client " + address + ":" + port + " data :" + message);
+
+                DatagramPacket sendPacket = new DatagramPacket(data, data.length, address, port);
+                socket.send(sendPacket);
+
+                System.out.println("sent client " + address + ":" + port + " data :" + message);
+
+            }
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
     }
+
 }
